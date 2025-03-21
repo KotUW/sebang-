38670c3097d99e24c4bb2fdf5f59cdf9ca566ecd
kotuw <46974369+KotUW@users.noreply.github.com>
2025-03-21 12:39:16 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+//go:embed "public/index.html"
+var index []byte
+
+func handleSearch(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	query := req.Form.Get("q")
+
+	// fmt.Println("Got query params: ", req.RequestURI, query, req.Form)
+
+	if query == "" {
+		http.Error(w, "Empty Query", http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, req, getSearchUrl(query), http.StatusFound)
+}
+
 func main() {
-	println("Hello!")
+	fmt.Println("Started Server at ::1:8080")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, string(index))
+	})
+
+	http.HandleFunc("/search", handleSearch)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getSearchUrl(query string) string {
+	res := fmt.Sprintf("https://google.com/search?q=%s", url.PathEscape(query))
+	return res
 }

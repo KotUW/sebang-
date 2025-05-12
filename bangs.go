b8bac837d985eab2ed46d4df6fee9f604bb26651
kotuw <46974369+KotUW@users.noreply.github.com>
2025-05-12 07:03:02 +0530

    Fix broken deploy

--- bangs.go
+++ bangs.go
@@ -1,3 +1,4 @@
 // First we will do bangs as a json file.
 //
 // Then use redis robably for user cokkie thing as, we will need to store user info too. (will also test fly.io persistance).
+package main
